da766c1536abb9e3c1106b53b29dc140a2a7a9bb
Unknown <zephyrmahe@msn.com>
2017-11-09 16:57:43 +0100

    reussite envoi message

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,34 @@
+var authKey = "";
 function receptionId(){
 	$.ajax({
 		url: 'http://messenger.api.niamor.com/createUser'
 	}).done(CUse);
 }
-// console.log($.ajax);
+console.log(authKey);
 	
+var myUser;
 
-function CUse(id_user){
-	console.log(id_user);
+function CUse(user){
+	myUser = user;
+	console.log(user);
+	console.log(myUser.authKey);
+	console.log(myUser.id);
+	console.log(myUser.createdAt);
+	console.log(myUser.lastMessageAt);
+	console.log(myUser.username);
 }
 receptionId();
 
-function btnMessage(){
-	$message = document.getElementById('leMessage').value; 
+function btnEnvoyer(){
+	$message = document.getElementById('leMessage').value;
+	$.ajax({
+		url: 'http://messenger.api.niamor.com/sendMessage',
+        method: 'post',
+        data: {
+        	authKey: myUser.authKey,
+        	text: $message,
+        	to: 0
+        }
+        
+	})
 }
