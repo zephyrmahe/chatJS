9e56ee0c67d9b168e1202b843ecd2891d36d4cf5
Unknown <zephyrmahe@msn.com>
2017-11-09 16:31:28 +0100

    debut ajout de fonction envoyer message

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,16 @@
-function btnEnvoyer(){
+function receptionId(){
 	$.ajax({
 		url: 'http://messenger.api.niamor.com/createUser'
 	}).done(CUse);
 }
 // console.log($.ajax);
-	// $message = document.getElementById('leMessage').value; 
+	
 
 function CUse(id_user){
 	console.log(id_user);
+}
+receptionId();
+
+function btnMessage(){
+	$message = document.getElementById('leMessage').value; 
 }
