9c49ff7272bc85e99cbec435270b6d779bb7247e
Unknown <zephyrmahe@msn.com>
2017-11-13 11:07:01 +0100

    ++

--- javascript/script.js
+++ javascript/script.js
@@ -1,52 +1,52 @@
 var authKey = "";
 function receptionId(){
-	$.ajax({
-		url: 'http://messenger.api.niamor.com/createUser'
-	}).done(CUse);
+  $.ajax({
+    url: 'http://messenger.api.niamor.com/createUser'
+  }).done(CUse);
 }
-	
+  
 var myUser;
 
 function CUse(user){
-	myUser = user;
+  myUser = user;
 }
 receptionId();
 
 function btnEnvoyer(){
-	$message = document.getElementById('leMessage').value;
-	$.ajax({
-		url: 'http://messenger.api.niamor.com/sendMessage',
+  $message = document.getElementById('leMessage').value;
+  $.ajax({
+    url: 'http://messenger.api.niamor.com/sendMessage',
         method: 'post',
         data: {
-        	authKey: myUser.authKey,
-        	text: $message,
-        	to: 0
+          authKey: myUser.authKey,
+          text: $message,
+          to: 0
         }
         
-	})
+  })
 }
 var othUse;
 
 function messRecu(){
-	$.ajax({
-		url: 'http://messenger.api.niamor.com/getMessages',
-		method: 'post',
-		data: {
-			authKey: myUser.authKey,
-			lastId: 0
-		}
+  $.ajax({
+    url: 'http://messenger.api.niamor.com/getMessages',
+    method: 'post',
+    data: {
+      authKey: myUser.authKey,
+      lastId: 0
+    }
 
-	}).done(affichMess)
+  }).done(affichMess)
 
 }
 
 function affichMess(oUse){
-	document.getElementById('messRecus').innerHTML = "";
-	
+  document.getElementById('messRecus').innerHTML = "";
+  
 for (i = 0; i < oUse.length; i++) {
-	othUse = oUse[i];
-	document.getElementById('messRecus').innerHTML += othUse.from.username+" : "+othUse.text+"<br>";
-	}
+  othUse = oUse[i];
+  document.getElementById('messRecus').innerHTML += othUse.from.username+" : "+othUse.text+"<br>";
+  }
 }
 
 $(document).ready(setInterval(messRecu, 30));
